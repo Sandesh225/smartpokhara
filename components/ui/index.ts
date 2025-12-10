9989c2a1184827ffa6f5a4e33706354889a17ac9
Sandesh Subedi <subedi.sandesh888@gmail.com>
2025-12-10 21:48:51 +0545

    Remove outdated UI barrel export file no longer required

--- components/ui/index.ts
+++
@@ -1,8 +0,0 @@
-// components/ui/index.ts
-export * from "./button";
-export * from "./card";
-export * from "./tabs";
-export * from "./table";
-export * from "./badge";
-export * from "./input";
-export * from "./select";
