7d51423336236a903e170efae630b284b855d289
Sandesh Subedi <subedi.sandesh888@gmail.com>
2026-01-08 21:32:45 +0545

    Update admin layout

--- app/(protected)/admin/layout.tsx
+++ app/(protected)/admin/layout.tsx
@@ -26,7 +26,7 @@ export default async function AdminLayout({ children }: AdminLayoutProps) {
   return (
     <div className="min-h-screen bg-gray-50">
       <AdminNavigation />
-      <main className="pl-64 pt-16">
+      <main className="pl-60 pt-6">
         <div className="p-6">{children}</div>
       </main>
     </div>
